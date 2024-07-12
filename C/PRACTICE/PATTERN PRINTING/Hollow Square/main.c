75e9889928fee06b34aa1cb151cbe4a8150ad804
milanmurali <milanmurali60@gmail.com>
2024-07-12 12:33:43 +0530

    12/7 Z pattern

---
+++ C/PRACTICE/PATTERN PRINTING/Hollow Square/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+
+    printf("Enter number of Rows: ");
+    scanf("%d", &n);
+
+
+
+    return 0;
+}
---
+++ C/PRACTICE/PATTERN PRINTING/I/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+
+    printf("Enter number of Rows: ");
+    scanf("%d", &n);
+
+    // first line
+    for (j = 0; j <= n; j++)
+    {
+        printf("* ");
+    }
+    printf("\n");
+
+    // diag
+    for (i = n - 1; i >= 1; i--)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            printf(" ");
+        }
+        printf("*  \n");
+    }
+
+    // last line
+    for (j = 0; j <= n; j++)
+    {
+        printf("* ");
+    }
+    printf("\n");
+
+    return 0;
+}
