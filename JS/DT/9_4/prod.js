82515f21a87cac369ddbbf2762a687f09d7b77cb
milanmurali <milanmurali60@gmail.com>
2024-09-04 10:41:10 +0530

    sep4

---
+++ JS/DT/9_4/prod.js
@@ -0,0 +1,32 @@
+!  Solve the Javascript Question using Array Methods 
+
+
+products=[
+    {pid:100,pName:'apple',band:'5g',price:120000,display:'led'},
+    {pid:101,pName:'samsaung',band:'5g',price:45000,display:'led'},
+    {pid:102,pName:'blackberry',band:'4g',price:50000,display:'led'},
+    {pid:103,pName:'nokia',band:'3g',price:1200,display:'lcd'},
+    {pid:104,pName:'motorola',band:'4g',price:10000,display:'lcd'}
+]
+console.log('-------------------------------------------------------------')
+//1. print product name only
+
+// console.log(products);
+
+console.log('-------------------------------------------------------------')
+
+//2. print all mobile details whose display is lcd
+console.log('-------------------------------------------------------------')
+
+
+//3. print 5g mobile phone name
+console.log('-------------------------------------------------------------')
+//4. filter mobile based on price
+console.log('--------------------------------------------------------------');
+
+//5. print costly mobile
+console.log('---------------------------------------------------------------')
+
+//6. print low cost mobile
+console.log('---------------------------------------------------------------');
+
