a2328271c7dbffcbe9c0665822e5193f1a96b26b
milanmurali <milanmurali60@gmail.com>
2024-12-13 12:12:55 +0530

    nodeuploaddelete

---
+++ Node/controllers/useruploadcontroller.js
@@ -0,0 +1,61 @@
+import { request, response } from "express";
+import user from "../models/useruploadschema.js"
+
+
+const add = async (req, res) => {
+    let newuser = new user(req.body)
+    let response = await newuser.save()
+    res.json(response)
+    console.log(response);
+
+}
+
+const view = async (req, res) => {
+    let response = await user.find()
+    res.json(response)
+}
+
+const viewid = async (req, res) => {
+    let id = req.params.id
+    let response = await user.findById(id)
+    res.json(response)
+}
+
+const update = async (req, res) => {
+    let id = req.params.id
+    let response = await user.findByIdAndUpdate(id, req.body)
+    res.json(response)
+}
+
+const deletee = async (req, res) => {
+    let id = req.params.id
+    let response = await user.findByIdAndDelete(id)
+    res.json(response)
+}
+
+const login = async (req, res) => {
+    const { email, password } = req.body
+    try {
+        let users = await user.findOne({ email: email })
+        if (!users) {
+            console.log("Ingane oruthan illa");
+            return res.status(404).json({ message: "Ingane oruthan illa" })
+        }
+        if (users.password === password) {
+            console.log("Mmm kerikko")
+            return res.json(users);
+        }
+        else {
+            console.log("Noki Type cheyy");
+            return res.status(401).json({ message: "Noki Type cheyy" })
+        }
+    }
+    catch (error) {
+        console.error("Entho Kozppand", error);
+        return res.status(500).json({ message: "Entho Kozppand" })
+
+    }
+}
+
+
+export { add, view, viewid, update, deletee, login }   
---
+++ Node/routes/useruploadrouter.js
@@ -0,0 +1,17 @@
+import express from "express"
+import { add, deletee, login, update, view, viewid } from "../controllers/useruploadcontroller.js"
+
+const useruploadrouter = express.Router()
+
+useruploadrouter.post("/add", add)
+useruploadrouter.post("/login", login)
+
+
+useruploadrouter.get("/view", view)
+useruploadrouter.get("/viewid/:id", viewid)
+
+useruploadrouter.put("/update/:id", update)
+
+useruploadrouter.delete("/delete/:id", deletee)
+
+export default useruploadrouter
