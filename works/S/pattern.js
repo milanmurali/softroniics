05e0f4535598742de4718369cfb36bb080c8b5bf
milanmurali <milanmurali60@gmail.com>
2025-05-09 21:31:48 +0530

    Remove works directory from repository

--- works/S/pattern.js
+++
@@ -1,9 +0,0 @@
-let rows = 8
-
-for (let i = 0; i < rows; i++) {
-    let space = ' ';
-
-    for(let j=0;j<rows-1;j++){
-        space = space + " "
-    }
-}
--- works/TrackEXP/js.js
+++
@@ -1,2 +0,0 @@
-const newset = new set([1, 2, 3, 4, 4])   //duplicates will be ignored
-console.log(newset)
